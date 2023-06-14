225e1acc927d6fb1c762ac1e7e66b372a2ba6547
Hassan <hasanpak97@gmail.com>
2023-06-14 03:42:57 -0700

    First Commit TableView controller setup

---
+++ Todoey/ToDoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController : UITableViewController {
+
+    let items = ["Grocery","Car Tuning","Reading book","ready for lecture"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",for: indexPath)
+        cell.textLabel?.text=items[indexPath.row]
+        return cell
+    }
+
+}
+
